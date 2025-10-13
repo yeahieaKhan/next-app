e392b088ae833d4e73b0d5e341749290a52ecd91
yeahieaKhan <yeahieakhan2017@gmail.com>
2025-10-13 23:13:11 +0600

    navbar done

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +23,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Navbar></Navbar>
+        <main className="h-screen h-max-[600px]">{children}</main>
       </body>
     </html>
   );
